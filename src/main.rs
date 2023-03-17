efc1ec3dee445079f9384dd2efdeff12c9d2ac85
Jack Contro <jack.contro@gmail.com>
2023-03-17 11:01:44 +0000

    improved the readability of the application

--- src/main.rs
+++ src/main.rs
@@ -43,16 +43,16 @@ fn main() -> Result<(), anyhow::Error> {
 
     // The WAV file we're recording to.
     const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
-    println!("Recording to {}", PATH);
-    let spec = wav_spec_from_config(&config);
-    let writer = hound::WavWriter::create(PATH, spec)?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
+    // println!("Recording to {}", PATH);
+    // let spec = wav_spec_from_config(&config);
+    // let writer = hound::WavWriter::create(PATH, spec)?;
+    // let writer = Arc::new(Mutex::new(Some(writer)));
 
     // A flag to indicate that recording is in progress.
-    println!("Begin recording...");
+    // println!("Begin recording...");
 
     // Run the input stream on a separate thread.
-    let writer_2 = writer.clone();
+    // let writer_2 = writer.clone();
 
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
@@ -61,25 +61,24 @@ fn main() -> Result<(), anyhow::Error> {
     let stream = match config.sample_format() {
         cpal::SampleFormat::I8 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<i8, i8>(data, &writer_2),
+            move |data, _: &_| print_input_data(data),
             err_fn,
             None,
         )?,
         cpal::SampleFormat::I16 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
+            move |data, _: &_| print_input_data(data),
             err_fn,
             None,
         )?,
         cpal::SampleFormat::I32 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<i32, i32>(data, &writer_2),
+            move |data, _: &_| print_input_data(data),
             err_fn,
             None,
         )?,
         cpal::SampleFormat::F32 => device.build_input_stream(
             &config.into(),
-            // move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
             move |data, _: &_| print_input_data(data),
             err_fn,
             None,
@@ -96,8 +95,8 @@ fn main() -> Result<(), anyhow::Error> {
     // Let recording go for roughly three seconds.
     std::thread::sleep(std::time::Duration::from_secs(1));
     drop(stream);
-    writer.lock().unwrap().take().unwrap().finalize()?;
-    println!("Recording {} complete!", PATH);
+    // writer.lock().unwrap().take().unwrap().finalize()?;
+    // println!("Recording {} complete!", PATH);
 
     // let mut reader = hound::WavReader::open(PATH)?;
     // let samples: Vec<f32> = reader.samples().map(|s| s.unwrap()).collect();
@@ -126,26 +125,8 @@ fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec 
 
 type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
 
-fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
-where
-    T: Sample,
-    U: Sample + hound::Sample + FromSample<T>,
-{
-    if let Ok(mut guard) = writer.try_lock() {
-        if let Some(writer) = guard.as_mut() {
-            for &sample in input.iter() {
-                
-                let sample: U = U::from_sample(sample);
-                // let display_sample = sample.to_f32();
-                writer.write_sample(sample).ok();
-
-            }
-        }
-    }
-}
 
 fn print_input_data(input: &[f32]) {
-    for &sample in input.iter() {
-        println!("Sample: {}", sample);
-    }
+    println!("Input data: {:?}", input);
+    panic!("Stop recording")
 }
